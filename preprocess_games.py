da0fc7be0dd20d1790e44a0fbc9a8bfa1348f519
Hairan Liang <65080149+hairanliang@users.noreply.github.com>
2024-12-26 16:33:03 -0500

    Delete preprocess_games.py

--- preprocess_games.py
+++
@@ -1,9 +0,0 @@
-# Open the file for writing
-
-with open('/Users/hairanliang/Documents/Chess.com/MyGames/allPgns copy.txt', 'r') as file:
-    filedata = file.read()
-
-filedata = filedata.replace('@', '')
-
-with open('/Users/hairanliang/Documents/Chess.com/MyGames/allPgns copy.txt', 'w') as file:
-    file.write(filedata)
