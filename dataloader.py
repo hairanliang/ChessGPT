1ee2faf8a27d7a83f61397fe3f653f4a45f187fe
Hairan Liang <65080149+hairanliang@users.noreply.github.com>
2024-12-26 16:34:05 -0500

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -5,54 +5,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 from util import produce_pairs
 import torch
+import pickle
 
 # Design choice: have the dataloader call the utility function which then handles the input target pairs here
 
-# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-# tokenizer.pad_token = tokenizer.eos_token
-
-
 # What will be fed in is a list of moves, for which all that has to be done is convert them to the token through a simple for loop
 class ChessDataset(Dataset):
-    def __init__(self, games, token_dict, max_length):
+    def __init__(self, game_list, token_dict, max_game_length):
         # print("initialized")
-        self.games = games
+        self.game_list = game_list
         self.token_dict = token_dict # This will just be a dictionary mapping from chess move to integer(Long dtype)
-        self.max_length = max_length
+        self.max_game_length = max_game_length + 1 # Adjustment for special tokens
 
     def __len__(self):
         # print("inside len")
-        # print(f"len: {len(self.games)}")
-        return len(self.games)
+        # print(f"len: {len(self.game_list)}")
+        return len(self.game_list)
 
     def __getitem__(self, idx):
         # print("inside getitem")
         # Remember, this is applied to one element of the batch, it doesn't see the other batch elements here
         prepend = 'BOS'
         
-        game = self.games[idx].copy() # Have to make sure don't mutate it with each access, so just make a copy
-        print(f"game before: {game}")
+        game = self.game_list[idx].copy() # Have to make sure don't mutate it with each access, so just make a copy
+        # print(f"game before: {game}")
         game = [prepend] + game
-    
-        print(f"game: {game}")
-        # Now, need to append the EOS token to the end of the tokenized game: if len() < max_length + 1, then add until it is max_length + 1
-        if len(game) < self.max_length + 1:
-            while len(game) < self.max_length + 1:
-                game.append('EOS')
-        elif len(game) > self.max_length + 1:
-            game = game[:self.max_length+1]
-        # Somewhere in between here have to apply the utility functions
+
+        # Got my logic all wrong! Need to have the EOS token first, then have the pad tokens come AFTER to fill out the rest
+
+        # print(f"game: {game}")
+        # Now, need to append the PAD token to the end of the tokenized game: if len() < max_length + 1, then add until it is max_length + 1
+        if len(game) >= self.max_game_length:
+            game = game[:self.max_game_length - 1]
+            game.append('EOS')
+        else:
+            game.append('EOS')
+            while len(game) < self.max_game_length:
+                game.append('PAD')
+
+        # print(f"game: {game}")
         tokenized_game = self.tokenize(game, self.token_dict)
 
         # print(f"Tokenized game: {tokenized_game}")
         # print(f"shape of tokenized game: {tokenized_game.shape}")
-        
-        EOS_token = len(self.token_dict) - 1
-        loss_mask = [0 if token == EOS_token else 1 for token in tokenized_game]
 
-        loss_mask = torch.tensor(loss_mask[:-1]) # This is to correct for the fact that the 
-        return tokenized_game, loss_mask # attention mask (encoder-only since we pad on the right anyway) not only helps for attention but also will be useful for calculating loss to ignore padded tokens
+        PAD_token = len(self.token_dict) - 2 # PAD token is second last token
+        EOS_token = len(self.token_dict) - 1 # EOS token is last token
+        loss_mask = torch.tensor([0 if token == PAD_token else 1 for token in tokenized_game])
+
+        # print(f"PAD_token: {PAD_token}")
+        # print(f"EOS_token: {EOS_token}")
+        # print(f"token_dict: {token_dict}")
+        # print(f"tokenized_game: {tokenized_game}")
+        # print(f"original loss_mask: {loss_mask}")
+
+        loss_mask = loss_mask[:-1] # This is to correct for the fact that we grabbed max_length + 1 tokens, since we are using the produce_pairs function
+        # print(f"shifted loss_mask: {loss_mask}")
+        # print(f"length of game: {len(game)}")
+        return tokenized_game, loss_mask # loss mask used for calculating loss to ignore padded tokens; for encoder could be used to ignore padding in attention calculation I believe
     
     def tokenize(self, game, token_dict):
         # print("inside tokenize")
@@ -61,56 +71,55 @@ class ChessDataset(Dataset):
             if move not in token_dict:
                 print(f"Move '{move}' not found in token_dict!")
             game[i] = token_dict[move]
-            # print("error is in return statement")
         return torch.tensor(game)
                         
     
 if __name__ == '__main__':
 
-    file = "/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn"
-    f = open(file)
-
     game_list = []
-    counter = 0
-    token_dict = {} # This will act as the tokenizer. 
+    token_dict = {} # This will act as the tokenizer/encoder
+    reverse_dict = {} # This will act as the decoder from tokens to string/SAN 
     game_counter = 0
-    while True:   # In the future, can think about modularizing this somehow
-        game = chess.pgn.read_game(f)
-        if game is None:
-            break  # end of file
-        
-        moves_list = []
-        
-        board = game.board()  
-        for move in game.mainline_moves():
-            san_move = board.san(move)
-            # print(san_move)
-            moves_list.append(san_move)
 
-            if san_move not in token_dict:
-                token_dict[san_move] = counter
-                counter += 1
-            
-            board.push(move)
-        
-        game_list.append(moves_list) # This should be a list of lists, where each list is one chess game in standard notation (SAN), each move is one element in the list, no more numbers
-        game_counter += 1
+    with open('LichessElite/game_list.pkl', 'rb') as file:
+        game_list = pickle.load(file)
 
-    token_dict['EOS'] = len(token_dict) # Adding in an EOS/padding token
+    with open('LichessElite/token_dict.pkl', 'rb') as file:
+        token_dict = pickle.load(file)
+
+    with open('LichessElite/reverse_dict.pkl', 'rb') as file:
+        reverse_dict = pickle.load(file)
+
+    with open('LichessElite/counter.pkl', 'rb') as file:
+        game_counter = pickle.load(file)
+
+    # After edits, this will all be taken care of in the token_dict
+
+    # Set first token to 0/'BOS'
+    # token_dict['BOS'] = 0 
+    # reverse_dict[0] = 'BOS'
+
+    # pad_token = len(token_dict)
+    # token_dict['PAD'] = pad_token # Adding in a padding token
+    # reverse_dict[pad_token] = 'PAD'
+
+    # eos_token = len(token_dict)
+    # token_dict['EOS'] = eos_token # Adding in an EOS token
+    # reverse_dict[eos_token] = 'EOS'
 
     vocab_size = len(token_dict)
     print(f"number of games: {game_counter}")
     print(f"tokenizer dict: {token_dict}")
+    print(f"decoder dict: {reverse_dict}")
     print(f"vocab_size: {vocab_size}")
-    print(f"game list: {game_list}")
 
-
-    ds = ChessDataset(game_list, token_dict, max_length=120)
+    ds = ChessDataset(game_list, token_dict, max_game_length=120)
 
     print(f"length of ds inside main: {len(ds)}")
     print(f"first ds element inside main{ds[0]}")
     print(ds[0])
     
+
 
 
 
